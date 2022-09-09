093fe6ecf5045914a9d78ef21e6223e8bc4dca93
Hüseyn Alıyev <huseynma@code.edu.az>
2022-09-09 15:03:45 +0400

    feat: Added UserSetting Page

--- src/pages/Setting/Setting.tsx
+++ src/pages/Setting/Setting.tsx
@@ -1,54 +1,93 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Input from '../../components/UI/Input';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Input from "../../components/UI/Input";
+import Button from "../../components/UI/Button";
 
 const Setting = () => {
-    const sidebarIsActive = useSelector((state:any) => state.sidebarToggle.isActive);
-
-    
+  const sidebarIsActive = useSelector(
+    (state: any) => state.sidebarToggle.isActive
+  );
 
   return (
-    <section className={`setting-section ${!sidebarIsActive && "sidebar-notactive"}`}>
-        <div className='container'>
-            <div className='row'>
-                <div className='col-lg-12'>
-                    <div className='setting-section__user'>
-                        <div className='user-info'>
-                            <div className='user-avatar'>
-                                <img src="https://wordpress.iqonic.design/product/wp/socialv/wp-content/uploads/avatars/33/1656654204-bpfull.jpg" />
-                            </div>
-                            <div className='user-fullname'>
-                                <h5>Marvin McKinney</h5>
-                                <span>Member since 2022</span>
-                            </div>
-                        </div>
-                        <div className='user-logout'>
-                            <ul>
-                                <li><Link to="/newsfeed"><i className="fa-solid fa-house-user"></i></Link></li>
-                                <li><Link to="/"><i className="fa-solid fa-arrow-right-from-bracket"></i></Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className='setting-section__personal-info'>
-                        <h4>Personal Information</h4>
-                        <form>
-                            <Input type="text" id="firstname" placeholder="Firstname" />
-                            <Input type="text" id="lastname" placeholder="Lastname" />
-                            <select>
-                                <option>Male</option>
-                                <option>Female</option>
-                            </select>
-                        </form>
-                    </div>
-                    <div className='account-section__account-setting'>
-
-                    </div>
+    <section
+      className={`setting-section ${!sidebarIsActive && "sidebar-notactive"}`}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="setting-section__user">
+              <div className="user-info">
+                <div className="user-avatar">
+                  <img src="https://wordpress.iqonic.design/product/wp/socialv/wp-content/uploads/avatars/33/1656654204-bpfull.jpg" />
                 </div>
+                <div className="user-fullname">
+                  <h5>Marvin McKinney</h5>
+                  <span>Member since 2022</span>
+                </div>
+              </div>
+              <div className="user-logout">
+                <ul>
+                  <li>
+                    <Link to="/newsfeed">
+                      <i className="fa-solid fa-house-user"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
+            <div className="setting-section__personal-info">
+              <h4>Personal Information</h4>
+              <form>
+                <div className="form-fullname">
+                  <div className="col-lg-5">
+                    <Input
+                      type="text"
+                      id="firstname"
+                      placeholder="Firstname"
+                      label="Type Firstname"
+                      mainDivClass="form-firstname"
+                    />
+                  </div>
+                  <div className="col-lg-5">
+                    <Input
+                      type="text"
+                      id="lastname"
+                      placeholder="Lastname"
+                      label="Type Lastname"
+                      mainDivClass="form-lastname"
+                    />
+                  </div>
+                </div>
+
+                <div className="form-username">
+                  <div className="col-lg-10">
+                    <Input
+                      type="text"
+                      id="username"
+                      placeholder="Username"
+                      label="Type Username"
+                    />
+                  </div>
+                </div>
+                <Button
+                  type="submit"
+                  innerText="Save Changes"
+                  className="btn"
+                />
+              </form>
+            </div>
+            <div className="account-section__account-setting"></div>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Setting;
